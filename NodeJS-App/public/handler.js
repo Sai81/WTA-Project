7effff247369a3235b501bfa8254bfab8647c5c5
Sai40481 <saisrivatsa81@gmail.com>
2021-04-27 18:44:39 +0530

    final

--- NodeJS-App/public/handler.js
+++ NodeJS-App/public/handler.js
@@ -1,31 +1,32 @@
 const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
-const roomContainer = document.getElementById('room-container')
+const msg_form = document.getElementById('send_cont')
+const msg_input = document.getElementById('msg_input')
+const room_array = document.getElementById('room-container')
+const msg_container = document.getElementById('msg_container')
 
-if (messageForm != null) {
+
+if (msg_form != null) {
   const name = prompt('You are entering a public room. Enter the name you want to be identified by.',"Anonymous")
   appendMessage('<center><by>You</by> joined</center>')
-  socket.emit('new-user', roomName, name)
+  socket.emit('new-user', room, name)
 
-  messageForm.addEventListener('submit', e => {
+  msg_form.addEventListener('submit', e => {
     e.preventDefault()
-    const message = messageInput.value
-    appendMessage(`<by>You</by><br>${message}`)
-    socket.emit('send-chat-message', roomName, message)
-    messageInput.value = ''
+    const message = msg_input.value
+    appendMessage(`<by>You</by><br>${msg}`)
+    socket.emit('send-chat-message', room, message)
+    msg_input.value = ''
   })
 }
 
 socket.on('room-created', room => {
-  const roomElement = document.createElement('div')
-  roomElement.innerText = room
-  const roomLink = document.createElement('a')
-  roomLink.href = `/${room}`
-  roomLink.innerText = 'join'
-  roomContainer.append(roomElement)
-  roomContainer.append(roomLink)
+  const new_room = document.createElement('div')
+  new_room.innerText = room
+  const link_to_room = document.createElement('a')
+  link_to_room.href = `/${room}`
+  link_to_room.innerText = 'join'
+  room_array.append(new_room)
+  room_array.append(link_to_room)
 })
 
 socket.on('chat-message', data => {
@@ -43,5 +44,5 @@ socket.on('user-disconnected', name => {
 function appendMessage(message) {
   const messageElement = document.createElement('div')
   messageElement.innerHTML = message
-  messageContainer.append(messageElement)
+  msg_container.append(messageElement)
 }
